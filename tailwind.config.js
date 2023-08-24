8119777ff73bdd654e3d7ed225667dfd86690c17
dorianinc <dorianrdba@gmail.com>
2023-08-23 17:27:39 -0700

    added responsiveness to site

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   content: ["./src/*.{html,js}", "node_modules/preline/dist/*.js"],
   theme: {
     extend: {
+      height: {
+        "30-rem": "25rem",
+      },
       width: {
-        '49': '49%',
-      }
+        "49-percent": "49%",
+        "60-rem": "60rem",
+        "70-rem": "70rem",
+        "75-rem": "75rem",
+      },
     },
   },
   plugins: [require("preline/plugin")],
