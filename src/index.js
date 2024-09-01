58bab3010c699da9fb7d0ccf1b59d5ffa75a0805
Dorian Macias <dorianrdba@gmail.com>
2024-08-31 22:33:48 -0700

    updated portfolio to dynamically built pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-// hello world
+import initializePortfolio from "./js/portfolio.js";
+import createCalculator from "./js/calculator.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const pathName = window.location.pathname;
+  console.log("🖥️  pathName: ", pathName)
+
+  if (pathName === "/apps") {
+    initializeAppCounter();
+  } else {
+    initializePortfolio();
+  }
+});
+
+const initializeAppCounter = () => {
+  createCalculator();
+};
