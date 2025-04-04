c3468288416ba55ce93238dcb124e6fb4d56b035
Dorian Macias <dorianrdba@gmail.com>
2025-04-04 13:53:47 -0700

    updated tailwind file"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}", "node_modules/preline/dist/*.js"],
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./apps/**/*.{html,js,jsx}",
+  ], 
   theme: {
     extend: {
       height: {
